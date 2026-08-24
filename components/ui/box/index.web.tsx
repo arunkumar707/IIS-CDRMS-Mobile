fd450ffa45707c64030cfe8fd2fe887ec9cddcf3
Monisha76345 <monishasgowda0@gmail.com>
2026-08-24 16:59:36 +0530

    Web changes

--- components/ui/box/index.web.tsx
+++ components/ui/box/index.web.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { boxStyle } from './styles';
 
 import type { VariantProps } from '@gluestack-ui/utils/nativewind-utils';
 
 type IBoxProps = React.ComponentPropsWithoutRef<'div'> &
-  VariantProps<typeof boxStyle> & { className?: string };
+  VariantProps<typeof boxStyle> & {
+    className?: string;
+  };
 
 const Box = React.forwardRef<HTMLDivElement, IBoxProps>(function Box(
-  { className, ...props },
+  { className, style, ...props },
   ref
 ) {
+  const webStyle = StyleSheet.flatten(style);
+
   return (
-    <div ref={ref} className={boxStyle({ class: className })} {...props} />
+    <div
+      ref={ref}
+      className={boxStyle({ class: className })}
+      style={webStyle}
+      {...props}
+    />
   );
 });
 
 Box.displayName = 'Box';
+
 export { Box };
